ed4ced8842da8aa0f48b5d545808e6878fbca743
Guilherme Junqueira <figueiredoguiu@gmail.com>
2023-03-27 16:49:39 -0300

    Feat: Extrair somente dados interessantes

--- src/App.java
+++ src/App.java
@@ -3,6 +3,10 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
+import javax.swing.text.html.parser.Parser;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -10,16 +14,16 @@ public class App {
         //  fazer uma conexão HTTP e buscar os top filmes
 
         String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
-        URI endereco = URI.create(url);
+        URI adress = URI.create(url);
         HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpRequest request = HttpRequest.newBuilder(adress).GET().build();
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         String body = response.body();
-        System.out.println(body);
 
         // extrair só os dados que interessam (titulo, poster, nota)
 
-        
+        var parser = new JsonParser();
+        List<Map<String, String>> movieList = parser.parse(body);
 
         // Manipular e exibir os dados
 
